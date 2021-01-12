cdd3efea80f9b2acbc66a67f927af0a3d7de024e
symr1010 <bgzlud@gmail.com>
2021-01-12 17:05:07 +0900

    商品出品ページの表示及び商品の保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,16 @@
 class Item < ApplicationRecord
   with_options presence: true do
+    validates :image
     validates :product_name
     validates :description_of_item
-    validates :image
+    validates :price, presence: true
   end
 
-  with_options presence: true format: { with: /\A[0-9]+\z/, message: 'には半角数字を使用してください' } do
-    validates :price, numericality: true { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  end
+  validates_inclusion_of :price, in: 300..9_999_999
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
 
   with_options numericality: { other_than: 1 } do
-    validates :category_id
+    validates :category_id 
     validates :product_condition_id
     validates :shipping_charge_id
     validates :prefecture_id
