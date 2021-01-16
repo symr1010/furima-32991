e077f63a4c21d27ad493ac058d891981f361acb2
symr1010 <bgzlud@gmail.com>
2021-01-16 10:58:17 +0900

    商品情報更新及びユーザー制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new] # :edit, :destroy
+  before_action :authenticate_user!, only: [:new, :edit] # :destroy
+  before_action :move_to_index, only: [:edit]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -26,6 +27,12 @@ class ItemsController < ApplicationController
   end  
   
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end  
   end
 
   private
@@ -33,5 +40,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:product_name, :description_of_item, :category_id, :product_condition_id, :shipping_charge_id,
                                  :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 end
