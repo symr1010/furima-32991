f39c1ff077bb605207bb7aed91dd34af7f0f0663
symr1010 <bgzlud@gmail.com>
2021-01-20 19:02:20 +0900

    購入機能の購入の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items
-  resources :orders, only: :index
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end  
 end
+
+#resources :orders, only: [:index, :create]
