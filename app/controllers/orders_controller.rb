2fb691d2b187d60a47914ceff0ab160369686e44
symr1010 <bgzlud@gmail.com>
2021-01-22 17:23:52 +0900

    商品購入ページへの制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
   def index
     @order_address = OrderAddress.new
   end
@@ -38,5 +40,9 @@ class OrdersController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end
